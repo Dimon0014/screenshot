dec380cc0842ce23c6a0350badecee78c1e5d568
Dima Toropov <toropov0014@mail.ru>
2017-11-26 12:05:38 +0300

    на работе 26

--- Prozrachoe okno.py
+++ Prozrachoe okno.py
@@ -1,28 +1,27 @@
 import wx
 
 class MyFrame(wx.Frame):
-    def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title,size=(250, 250))
-        #topPanel = wx.Panel(self, -1)
-        #topPanel.SetBackgroundColour('green')
-        panel1 = wx.Panel(self, -1, style=wx.TRANSPARENT_WINDOW)
-        #panel1.SetTransparent(100)
-        #panel2 = wx.Panel(self, -1)
-        #panel2.SetBackgroundColour('gray')
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(panel1,1,flag = wx.EXPAND|wx.ALL)
-        #sizer.Add(panel2,1,flag = wx.EXPAND|wx.ALL)
+ def __init__(self):
+               wx.Frame.__init__(self, None, -1, "My Frame", size=(300, 300))
+               panel = wx.Panel(self, -1) # панель на которой распалагаются элементы управления, Frame - это просто рамка, -1 это индификатор через который панель и фрейм соеденяются
+               panel.SetBackgroundColour('#FF6400')
+               panel.SetBackgroundColour(wx.WHITE)
+               panel.Bind(wx.EVT_MOTION, self.OnMove) # связываем панель и событие MOTION(движение мыши по панели)
+                                                      # с обработчиком OnMove()
+               wx.StaticText(panel, -1, "Pos:", pos=(10, 12)) # label статичный текст - "Pos:"
+               self.posCtrl = wx.TextCtrl(panel, -1, "333", pos=(40, 10)) # создаем атрибут - окно Edit - текстовое
+                                                                          # окошко и называем его posCtrl
+#https://www.youtube.com/watch?v=cp1ZeMisTNo&list=PLejTrt5hn2r1uzZ53GDeUElXRkRFbUmQd&index=2
+               button=wx.Button(panel, label='Exit',pos=(170,10), size=(60,30)) # создали кнопку
 
-        #topPanel.SetSizer(sizer)
+               self.SetTransparent(150)
+ def OnMove(self, event): # Передаем функции событие(event) в данном случае wx.EVT_MOTION
+              pos = event.GetPosition()
+              self.posCtrl.SetValue("%s, %s" % (pos.x, pos.y))
 
-
-
-class MyApp(wx.App):
-     def OnInit(self):
-         frame = MyFrame(None, -1, 'frame')
-         frame.Show(True)
-         return True
-
-app = MyApp(0)
-app.MainLoop()
+if __name__ == '__main__':
+           app = wx.App()
+           frame = MyFrame()
+           frame.Show(True)
+           app.MainLoop()
--- Sravnenie01.py
+++ Sravnenie01.py
@@ -1,21 +1,44 @@
+# попиксельное сравнение
 import pyscreenshot as ImageGrab
 import os
+from time import clock, sleep
 import time
 
 x_pad=0
 y_pad=0
 def screenGrab():
-	b1 = (x_pad + 1, y_pad + 1, x_pad + 640, y_pad + 480)
-	im = ImageGrab.grab(b1)
-	
-	im.save(os.getcwd() + '\\Sna_pro_'+'.png', 'PNG')
-	return im
+    b1 = (x_pad + 1, y_pad + 1, x_pad + 64, y_pad + 48)
+    im = ImageGrab.grab(b1)
+    
+    im.save(os.getcwd() + '\\Sna_pro_'+'.png', 'PNG')
+    return im
+def pix_to_pix(x,y,im1, im2):
+    for i in range(0, x, 4):
+        for k in range(0, y, 4):
+            f_ = (i, k)
+            p = im1.getpixel(f_)
+            p2 = im2.getpixel(f_)
+            if p == p2:
+                print('x=', i, 'y=', k)
+                if f_==(x-4, y-4):
+                 
+                 print('у картинок пикселы равны')
+            else:
+                print('пикселы не равны')
+                break
 if __name__ == "__main__" :
- im = screenGrab()
- f_ = (0, 0)
- p=im.getpixel(f_)
- p2=im.getpixel((0, 0))
- if p==p2:
-	 print('пикселы равны')
- else:
-	 print('пикселы не равны')
+ im1 = screenGrab()
+ im2=im1
+ start1 = clock()
+ pix_to_pix(64, 48, im1, im2)
+ end1 = clock()
+ # print(first_ten_even)
+ print("Result (iterativ): выполняется за " + "\nФункция %1.10f секунд" % (end1 - start1))
+
+ # f_ = (638, 478)
+ # p=im1.getpixel(f_)
+ # p2=im2.getpixel((638, 478))
+ # if p==p2:
+ #     print('пикселы равны')
+ # else:
+ #     print('пикселы не равны')
